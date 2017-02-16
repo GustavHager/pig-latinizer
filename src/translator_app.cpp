a3c22d26bd9228d32555ec12e1df7e3b6803904b
Mattias Tiger <matti166@student.liu.se>
2017-02-16 15:50:55 +0100

    Solves Issue #2 - Many Words

--- src/translator_app.cpp
+++ src/translator_app.cpp
@@ -1,15 +1,31 @@
 #include <string>
 #include <iostream>
+#include <vector>
+#include <sstream>
 #include "Translator.hpp"
 
 int main(int argc, char** argv){
 
     Translator translator;
-    std::string input;
-
-    std::cin >> input;
-
-    std::cout << translator.translate(input) << '\n';
+    std::vector<std::string> words;
+    
+    std::string line;
+    getline(std::cin, line);
+    
+    std::istringstream str(line);
+    std::string word;
+    
+    while( str >> line ) {
+      words.push_back(word);
+    }
+      
+    for(int n = 0; n < words.size()-1; n++) {
+        std::cout << translator.translate(words[n]) << " ";
+    }
+    if (!words.empty()) {
+        std::cout << translator.translate(words.back());
+    }
+    std::cout << '\n';
 
     return 0;
 }
