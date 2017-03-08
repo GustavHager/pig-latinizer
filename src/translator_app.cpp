8b23f4b86c7f6eaae141db19309d85cd1779964a
Mattias Tiger <matti166@student.liu.se>
2017-03-08 11:22:02 +0100

    Added support for reading from files and writing to files. #3

--- src/translator_app.cpp
+++ src/translator_app.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
+#include <fstream>
 #include <future>
 #include "PigLatinTranslator.hpp"
 #include "InverseTranslator.hpp"
@@ -20,6 +21,10 @@ int main(int argc, char** argv){
     command.addArgument("-i", "--inverse",
                         "Translate the words from pig-latin to english.",
                         CLP::CommandLineParser::NO_PARAMETER);
+    command.addArgument("-f", "--file-input",
+                        "Use the file from the provided path as input.");
+    command.addArgument("-o", "--file-output",
+                        "Write result to specified path.");
     /*
     command.addArgument<int>("-s", "--space-padding",
                              "Pads each word with spaces at the end so that it is at least NUM characters long.",
@@ -44,7 +49,7 @@ int main(int argc, char** argv){
     }
     */
     
-
+    // Select translator (Pig Latin or Inverse Pig latin)
     std::vector<std::string> words;
     Translator * translator;
     if(command.isset("--inverse")) {
@@ -54,20 +59,31 @@ int main(int argc, char** argv){
     }
 
     // Read words from input
-    std::string line;
-    getline(std::cin, line);
-    
-    std::istringstream str(line);
     std::string word;
-        
-    while( str >> word ) {
-      words.push_back(word);
+    if(command.isset("--file-input")) {
+      // Read from file
+      std::ifstream inputFile;
+      inputFile.open(command.get<std::string>("--file-input"));
+      AssertM(inputFile.is_open(), "Could not open file.");
+      while( inputFile >> word ) {
+        words.push_back(word);
+      }
+      inputFile.close();
+    } else {
+      // Read from standard input
+      std::string line;
+      getline(std::cin, line);
+      
+      std::istringstream str(line);
+          
+      while( str >> word ) {
+        words.push_back(word);
+      }
     }
-
-    std::vector<std::string> translations;
-    translations.reserve(words.size());
       
     // Translate all words
+    std::vector<std::string> translations;
+    translations.reserve(words.size());
     if(command.isset("--parallel")) {
       // Parallel
       std::vector<std::future<std::string> > future_translations;
@@ -86,13 +102,24 @@ int main(int argc, char** argv){
     }
 
     // Output the result
-    for(int n = 0; n < words.size()-1; n++) {
-        std::cout << translations[n] << " ";
+    if(command.isset("--file-output")) {
+      // Write to file
+      std::ofstream outputFile;
+      outputFile.open(command.get<std::string>("--file-output"));
+      AssertM(outputFile.is_open(), "Could not open file.");
+      for (auto& word : translations) {
+        outputFile << word << " ";
+      }
+      outputFile.close();
+    } else {
+      for(int n = 0; n < words.size()-1; n++) {
+          std::cout << translations[n] << " ";
+      }
+      if (!words.empty()) {
+          std::cout << translations.back();
+      }
+      std::cout << '\n';
     }
-    if (!words.empty()) {
-        std::cout << translations.back();
-    }
-    std::cout << '\n';
 
     return 0;
 }
